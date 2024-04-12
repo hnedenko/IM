3ff3f8ff90f0dd5566f8cc514a8f5cb17e319ebe
hnedenko <73238004+hnedenko@users.noreply.github.com>
2024-04-12 09:18:40 +0300

    create model Exercise

--- ImproveMath/exercise/models.py
+++ ImproveMath/exercise/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Exercise(models.Model):
+
+    class Difficulty(models.TextChoices):
+        EASY = 'ES', _('Easy')
+        MEDIUM = 'MD', _('Medium')
+        HARD = 'HD', _('Hard')
+        EXTRA = 'EX', _('Extra')
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    difficulty = models.CharField(max_length=2, choices=Difficulty.choices, default=Difficulty.MEDIUM, blank=False)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + str(self.difficulty)
